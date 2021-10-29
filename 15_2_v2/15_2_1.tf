ffc3ad0f00f612bef1455cd0af7d95080cdcc6fd
i.siropyatov <i.siropyatov@gmail.com>
2021-10-29 16:05:34 +0500

    +update work м2

--- 15_2_v2/15_2_1.tf
+++ 15_2_v2/15_2_1.tf
@@ -1,26 +1,26 @@
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "b_suba" {
   bucket = "netology29102022"
   acl    = "public-read"
 
   tags = {
-    Name    = "My bucket"
+    Name    = "bucket"
   }
 }
 
 resource "aws_s3_bucket_object" "img" {
-  bucket = aws_s3_bucket.b.id
-  key    = "my_image.png"
+  bucket = aws_s3_bucket.b_suba.id
+  key    = "suba_image.png"
   source = "image.png"
   acl    = "public-read"
 
   etag = filemd5("image.png")
 }
 
-data "aws_s3_bucket" "b" {
-  bucket = aws_s3_bucket.b.id
+data "aws_s3_bucket" "b_suba" {
+  bucket = aws_s3_bucket.b_suba.id
 }
 
 data "aws_s3_bucket_object" "img" {
-  bucket = aws_s3_bucket.b.id
+  bucket = aws_s3_bucket.b_suba.id
   key    = aws_s3_bucket_object.img.id
 }
